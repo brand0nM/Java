9475e2e13d55ea5ebd02b279ead1cb40b1df11a8
Brandon Montalvo <79609464+brand0nM@users.noreply.github.com>
2022-06-11 13:07:05 -0600

    clean maps

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,16 @@
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+cities.forEach(function(city){
+    L.circleMarker(city.location,{radius: ((city.population - 200000)/100000), color:'violet', 
+        fill:true, fillColor:'teal', weight:.5}).addTo(map)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + 
+        city.population.toLocaleString('en-US') + "</h3>")
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
 streets.addTo(map);
-// test changes for git branches
